0e8bbfbaf3e3e200ae93d787d0432e7d010ca509
EvelynOct <evelyne.aritonang72@gmail.com>
2025-11-12 21:53:41 +0700

    Tutorial 8

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 import 'package:football_news/screens/newslist_form.dart';
+// Tutorial 8:
+import 'package:football_news/screens/news_entry_list.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -73,6 +75,19 @@ class LeftDrawer extends StatelessWidget {
               //     builder: (context) => NewsListPage(),
               //   ),
               // );
+            },
+          ),
+
+          // Tutorial 8:
+          ListTile(
+            leading: const Icon(Icons.add_reaction_rounded),
+            title: const Text('News List'),
+            onTap: () {
+              // Route to news list page
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+              );
             },
           ),
         ],
